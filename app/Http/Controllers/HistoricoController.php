7351b61f6282f8ec23ce56043a3f6f9a7b86937f
Marcos Ferreira <marcosferreira.developer@gmail.com>
2025-09-14 20:40:31 -0300

    (Initial Commit): Desenvolvendo a aplicação web

---
+++ app/Http/Controllers/HistoricoController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+
+class HistoricoController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $telefone = $request->input('telefone', '');
+
+        $cliente = Cliente::where('telefone', $telefone)->with('enviados', 'recebidos')->first();
+
+        if (!$cliente) {
+            return redirect()->back()->with('error', 'Cliente não encontrado.');
+        }
+
+        return view('frete.historico', [
+            'cliente' => $cliente,
+        ]);
+    }
+}
---
+++ app/Http/Controllers/RastreamentoController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Frete;
+use Illuminate\Http\Request;
+
+class RastreamentoController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $codigoRastreio = $request->input('codigo_rastreio', '');
+
+        $frete = Frete::where('codigo_rastreio', $codigoRastreio)
+                    ->with('etapas')
+                    ->first();
+
+        if (!$frete) {
+            return redirect()->back()->with(['error' => 'Código de rastreamento inválido.']);
+        }
+
+        return view('frete.rastreamento', [
+            'frete' => $frete,
+        ]);
+    }
+}
