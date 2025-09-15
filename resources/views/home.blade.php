a3c2af4e67ab70bb7dfa7213e1b728d76ec76bdc
Marcos Ferreira <marcosferreira.developer@gmail.com>
2025-09-15 05:49:41 -0300

    Adicionar função para aplicar máscara no campo de telefone

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,4 +37,20 @@
             <img src="{{ asset('entrega.webp') }}" alt="Placeholder" class="object-cover w-full h-full">
         </div>
     </div>
+
+    <script>
+        function aplicarMascaraTelefone(input) {
+            let valor = input.value.replace(/\D/g, '');
+            if (valor.length > 0) {
+                valor = "(" + valor;
+            }
+            if (valor.length > 3) {
+                valor = valor.slice(0, 3) + ") " + valor.slice(3);
+            }
+            if (valor.length > 10) {
+                valor = valor.slice(0, 10) + "-" + valor.slice(10);
+            } 
+            input.value = valor.slice(0, 15);
+        }
+    </script>
 </x-layout>
